80925d8061823192cdf0476f6b72cf0573a10b13
Camille Touron <ctouron@ptb-07008323.grenoble.inria.fr>
2025-03-05 15:00:15 +0100

    add VPPrecond networks from EDM paper

--- embedding_nets.py
+++ embedding_nets.py
@@ -168,7 +168,8 @@ class FakeFNet(torch.nn.Module):
 
 class GaussianNet(torch.nn.Module):
     """
-    Score network that mimics the true shape of the posterior score and learn 3 matrices A_t, B_t, C_t
+    Score network that mimics the true shape of the posterior score
+    in the Gaussian case and learn 3 matrices A_t, B_t, C_t.
     The forward path looks like A_t(theta + B_t x + C_t)
     """
     def __init__(self, hidden_dim, output_dim):
@@ -212,7 +213,7 @@ class GaussianNet(torch.nn.Module):
         return self.A_t(t).detach(), self.B_t(t).detach(), self.C_t(t).detach()
 
     def alpha_t(self,t):
-        log_alpha = 0.5 * 39.9 * (t**2) + 0.1 * t
+        log_alpha = 0.5 * 19.9 * (t**2) + 0.1 * t
         return torch.exp(-log_alpha)
     
     def forward(self, theta, x, t):
@@ -240,9 +241,10 @@ class GaussianNet(torch.nn.Module):
         C = self.C_t(t_tmp)
         #size (batch_size,2)
         #C = -self.alpha_t(t_tmp)**0.5*(self.cov_post@self.inv_cov_prior@self.mu_prior).repeat(t_tmp.shape[0],1)
+        # score = (A@theta[...,None])[:,:,0] + (B@x[...,None])[:,:,0] + C
         score = A@((theta[...,None])[:,:,0] + (B@x[...,None])[:,:,0] + C)[...,None]
         # size(batch size,2)
-    
+  
         if cut:
             return score[0,:,0]
         else:
@@ -252,7 +254,7 @@ class GaussianNet(torch.nn.Module):
 class GaussianNetAlpha(torch.nn.Module):
     """
     Score network that only learns the function alpha(t)
-    and return the score with this learnt function
+    and returns the score with this learnt function
     """
     def __init__(self, hidden_dim):
         super().__init__()
@@ -298,3 +300,176 @@ class GaussianNetAlpha(torch.nn.Module):
             return score[0,:,0]
         else:
             return score[:,:,0]
+
+
+class NonCondNet(torch.nn.Module):
+    """ 
+    Score network for non conditional distribution,
+    i.e. not adapted for posterior score estimation
+    """
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, hidden_dim, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, output_dim, bias=True)
+        )
+        
+    def forward(self, theta, x, t): 
+        # x is not used in the non conditional case
+        t_tmp=t       
+        if t.ndim==0:
+            t_tmp=torch.tensor([t])
+        t_tmp=t_tmp.unsqueeze(1)
+
+        if theta.ndim==1:
+            theta=theta.unsqueeze(0)
+        theta,t_tmp=broadcast(theta,t_tmp,ignore=1)
+
+        output = self.layers(torch.cat((theta, t_tmp),dim=-1))
+        return output
+
+class VPPrecondnoncond(torch.nn.Module):
+    """
+    Score network for the VP diffusion version in a non conditional case adapted from
+    Karras T. et al. (2022) "Elucidating the Design Space of Diffusion-based Generative Models"
+    Learn a denoiser D(theta,x,t), not the score directly. If the input is not scaled by s(t):
+
+        score(theta,x,t)= (D(theta,x,t) - theta)/sigma(t)^2 
+    
+    Otherwise, 
+
+        score(theta,x,t)= (D(theta/s(t),x,t) - theta/s(t))/(s(t)sigma(t)^2)
+    """
+    def __init__(self, theta_dim, 
+                 hidden_dim=32,
+                 beta_d=19.9,
+                 beta_min=0.1,
+                 M=1000,
+                 epsilon_t=1e-5):
+        super().__init__()
+        self.beta_d = beta_d
+        self.beta_min = beta_min
+        self.M = M # for the time embedding
+        self.epsilon_t = epsilon_t # minimal training time
+        self.sigma_min = float(self.sigma(epsilon_t))
+        self.sigma_max = float(self.sigma(1))
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(theta_dim + 1, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, theta_dim),
+        )
+    
+    def forward(self, theta, x, t): 
+        # x is not used in the non conditional case
+        theta = theta.to(torch.float32)
+        t = t.to(torch.float32)
+        # the variable t should receive the added noise at time t and not directly the time t
+        sigma=t   
+        if t.ndim==0:
+            sigma=torch.tensor([t])
+        sigma=sigma.unsqueeze(1)
+        # preconditioning to avoid variance explosion
+        c_skip = 1 # skip connection
+        c_out = -sigma # scale the network output
+        c_in = 1 / (sigma ** 2 + 1).sqrt() # scale the network input
+        c_noise = (self.M - 1) * self.sigma_inv(sigma) # condition noise input
+        theta, c_noise = broadcast(theta,c_noise,ignore=1)
+        F_x = self.model(torch.cat(((c_in * theta).to(torch.float32), c_noise),dim=1))
+        # return the denoiser not the score directly
+        D_x = c_skip * theta + c_out * F_x.to(torch.float32)
+        return D_x
+    
+    def sigma(self, t):
+        """return the noise schedule for the training/sampling phase"""
+        t = torch.as_tensor(t)
+        return ((0.5 * self.beta_d * (t ** 2) + self.beta_min * t).exp() - 1).sqrt()
+    
+    def sigma_inv(self, sigma):
+        """return the time corresponding to a specific noise"""
+        sigma = torch.as_tensor(sigma)
+        return ((self.beta_min ** 2 + 2 * self.beta_d * (1 + sigma ** 2).log()).sqrt() - self.beta_min) / self.beta_d
+    
+    def round_sigma(self, sigma):
+        return torch.as_tensor(sigma)
+
+
+class VPPrecond(torch.nn.Module):
+    """
+    Score network for the VP diffusion version in a **conditional** case adapted from
+    Karras T. et al. (2022) "Elucidating the Design Space of Diffusion-based Generative Models"
+    Learn a denoiser D(theta,x,t), not the score directly. If the input is not scaled by s(t):
+
+        score(theta,x,t)= (D(theta,x,t) - theta)/sigma(t)^2 
+    
+    Otherwise, 
+    
+        score(theta,x,t)= (D(theta/s(t),x,t) - theta/s(t))/(s(t)sigma(t)^2)
+    """
+    def __init__(self, theta_dim,
+                 x_dim, 
+                 hidden_dim=32,
+                 beta_d=19.9,
+                 beta_min=0.1,
+                 M=1000,
+                 epsilon_t=1e-5):
+        super().__init__()
+        self.beta_d = beta_d
+        self.beta_min = beta_min
+        self.M = M
+        self.epsilon_t = epsilon_t
+        self.sigma_min = float(self.sigma(epsilon_t))
+        self.sigma_max = float(self.sigma(1))
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(theta_dim + x_dim + 1, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, theta_dim),
+        )
+    
+    def forward(self, theta, x, t):
+        # x is not used in the non conditional case
+        cut=True if theta.ndim==1 else False
+        theta = theta.to(torch.float32)
+        t = t.to(torch.float32)
+        x = x.to(torch.float32)
+        # the variable t should receive the added noise at time t and not directly the time t
+        sigma=t       
+        if t.ndim==0:
+            sigma=torch.tensor([t])
+        sigma=sigma.unsqueeze(1)
+        # preconditioning to avoid variance explosion
+        c_skip = 1 # skip connection
+        c_out = -sigma # scale the network output
+        c_in = 1 / (sigma ** 2 + 1).sqrt() # scale the network input
+        c_noise = (self.M - 1) * self.sigma_inv(sigma) # condition noise input        
+        theta, x, c_noise = broadcast(theta,x, c_noise,ignore=1)
+        F_x = self.model(torch.cat(((c_in * theta).to(torch.float32), x, c_noise),dim=-1))
+        # return the denoiser not the score directly
+        D_x = c_skip * theta + c_out * F_x.to(torch.float32)
+        if cut:
+            return D_x[-1]
+        else:
+            return D_x
+    
+    def sigma(self, t):
+        """return the noise schedule for the training/sampling phase"""
+        t = torch.as_tensor(t)
+        return ((0.5 * self.beta_d * (t ** 2) + self.beta_min * t).exp() - 1).sqrt()
+    
+    def sigma_inv(self, sigma):
+        """return the time corresponding to a specific noise"""
+        sigma = torch.as_tensor(sigma)
+        return ((self.beta_min ** 2 + 2 * self.beta_d * (1 + sigma ** 2).log()).sqrt() - self.beta_min) / self.beta_d
+    
+    def round_sigma(self, sigma):
+        return torch.as_tensor(sigma)
